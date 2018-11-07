ba92a56bffdc79e8d5f175c8faf4a443c804b311
richardlee0211 <richardlee0211er@gmail.com>
2018-11-07 13:08:59 -0500

    first step to coding

---
+++ proj1/APEX1/test.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include<stdbool.h>
+
+int translate(){
+  FILE* sim = fopen("apex_sim", "r");
+  if(!sim){
+    return -1;
+  }
+  char ctemp=0;
+  char* line = NULL;
+  size_t len = 0;
+  ssize_t nread;
+
+  bool first=true;
+  while(!feof(sim)){
+    fscanf(sim, "%c", &ctemp);
+    if(ctemp == 0x00) {
+      if(first){
+        printf("\n");
+        first=false;
+      }
+      break;
+    }
+    printf("0x%X", ctemp);
+    first=true;
+  }
+}
+
+int main(){
+  translate();
+  /* test of getline */
+  /*
+  char *line=NULL;
+  size_t len=0;
+  int nread=0;
+  while((nread=getline(&line, &len, stdin)) != -1){
+    printf("%s, len: %lu\n", line, len);
+  }
+  */
+}
